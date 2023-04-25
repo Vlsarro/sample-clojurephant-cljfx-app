c150de87d82dfae914ac0c43c5bfb8f7fd70ac47
Vlsarro <drekmaro@gmail.com>
2023-04-25 13:06:33 +0300

    add code for interactive cljfx app

--- src/main/clojure/my/group/myapp.clj
+++ src/main/clojure/my/group/myapp.clj
@@ -1,7 +1,42 @@
 (ns my.group.myapp
+  (:require [cljfx.api :as fx])
   (:gen-class))
 
+(defonce *state (atom {:slider-value 50}))
+
+(defmulti event-handler :event/type)
+
+(defmethod event-handler ::set-slider-value [e]
+  (swap! *state assoc :slider-value (:fx/event e)))
+
+(defn slider-view [{:keys [min max value event]}]
+  {:fx/type :slider
+   :min min
+   :max max
+   :value value
+   :major-tick-unit max
+   :on-value-changed {:event/type event}
+   :show-tick-labels true})
+
+(defn root-view[{{:keys [slider-value]} :state}]
+  {:fx/type :stage
+   :showing true
+   :scene {:fx/type :scene
+           :root {:fx/type :h-box
+                  :spacing 10
+                  :children [{:fx/type slider-view
+                              :min 0
+                              :max 100
+                              :event ::set-slider-value
+                              :value slider-value}]}}})
+
+(def renderer
+  (fx/create-renderer
+   :middleware (fx/wrap-map-desc (fn [state]
+                                   {:fx/type root-view
+                                    :state state}))
+   :opts {:fx.opt/map-event-handler event-handler}))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (apply println "Hello," args))
+  (fx/mount-renderer *state renderer))
---
+++ src/main/java/SliderLabel.java
@@ -0,0 +1,5 @@
+import javafx.scene.control.Label;
+
+public class SliderLabel extends Label {
+    
+}
