3f322dba32fe5fced48978df68d75d6f2ee5ffc9
Vlsarro <drekmaro@gmail.com>
2023-05-02 19:52:09 +0300

    sync SliderView with cljfx app look

--- src/main/java/SliderView.java
+++ src/main/java/SliderView.java
@@ -6,18 +6,26 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.scene.control.Slider;
 import javafx.stage.Stage;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import components.SliderLabel;
 
 public class SliderView extends Application {
 
-    final Slider mainSlider = new Slider(0, 100, 40);
+    final double mainSliderMaxValue = 100.00;
+
+    final Slider mainSlider = new Slider(0, mainSliderMaxValue, 40.0);
+
     final SliderLabel mainSliderValueLabel = new SliderLabel(Double.toString(mainSlider.getValue()));
     final Label javaInfoLabel = new Label(
         "Hello, JavaFX " + System.getProperty("javafx.version") + ", running on Java " + System.getProperty("java.version") + "."
     );
 
-    public void setupListeners() {
+    public void setupSlider() {
+        mainSlider.setMajorTickUnit(mainSliderMaxValue);
+        mainSlider.setBlockIncrement(10);
+        mainSlider.setShowTickLabels(true);
+
         mainSlider.valueProperty().addListener(new ChangeListener<Number>() {
             public void changed(ObservableValue <? extends Number> ov,
                 Number oldVal, Number newVal) {
@@ -28,14 +36,16 @@ public class SliderView extends Application {
 
     @Override
     public void start(Stage stage) {
+        setupSlider();
+
         VBox vb = new VBox(5, javaInfoLabel, mainSliderValueLabel, mainSlider);
         vb.setAlignment(Pos.CENTER);
+        vb.setPadding(new Insets(10));
+        vb.setSpacing(10);
 
         Scene scene = new Scene(vb, 640, 480);
+
         stage.setScene(scene);
-
-        setupListeners();
-
         stage.show();
     }
 
